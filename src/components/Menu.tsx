22237f34f1908d192cb5de82377892faee818db2
Daniel Oyola <danieloyolaj@yahoo.com>
2025-02-07 16:30:49 -0400

    Created a big calendar for the student page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { role } from "@/lib/data";
 
 const menuItems = [
   {
@@ -126,15 +127,23 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
-          {i.items.map(item => (
-            <Link 
-              href={item.href} 
-              key={item.label} 
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight">
-              <Image src={item.icon} alt="menu-icon" width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span> {/**this is the label of the menu */}
-            </Link>
-          ))}
+          {i.items.map(item => {
+            if(item.visible.includes(role)){
+              return (
+                <Link 
+                  href={item.href} 
+                  key={item.label} 
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight">
+                  <Image src={item.icon} alt="menu-icon" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span> {/**this is the label of the menu */}
+                </Link>
+              )
+            }
+          }
+            
+          )
+        
+          }
         </div>
       ) )}
     </div>
