128219a00e0fc7818b0d7ce2f410cf883c42ec59
Daniel Oyola <danieloyolaj@yahoo.com>
2025-02-12 10:42:55 -0400

    created lists for classes, lessons, parents and subjects

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -1,38 +1,32 @@
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
-import { parentsData, role, } from "@/lib/data"
+import {lessonsData, role, subjectsData, } from "@/lib/data"
 import Image from "next/image"
 import Link from "next/link"
 
-type Parent ={
+type Lesson ={
   id:number,
-  name:string;
-  email?:string;
-  students:string[];
-  phone:string;
-  address:string;
+  subject:string;
+  class:string;
+  teacher:string;
+  
 }
 
 const columns = [
   {
-    header: "Info",
-    accessor: "info",
+    header: "Subject Name",
+    accessor: "name",
   },
   {
-    header: "Student Name",
-    accessor: "students" ,
+    header: "Class",
+    accessor: "class" ,
     className: "hidden md:table-cell",
   },
   {
-    header: "Phone",
-    accessor: "phone" ,
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Address",
-    accessor: "address" ,
-    className: "hidden lg:table-cell",
+    header: "Teacher",
+    accessor: "teacher" ,
+    className: "hidden md:table-cell",
   },
   {
     header: "Actions",
@@ -40,21 +34,14 @@ const columns = [
   },
 ]
 
-const ParentListPage = () => {
+const LessonListPage = () => {
 
   //Showing all the rows
-  const renderRow = (item:Parent) => (
+  const renderRow = (item:Lesson) => (
     <tr key={item.id} className="border-b border-gray-200 ever:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
-      <td className="flex items-center gap-4 p-4">
-        
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.students.join(",")}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
+      <td className="flex items-center gap-4 p-4">{item.subject}</td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
@@ -78,7 +65,7 @@ const ParentListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP  */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Lessons</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -99,7 +86,7 @@ const ParentListPage = () => {
 
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={parentsData}/>
+      <Table columns={columns} renderRow={renderRow} data={lessonsData}/>
       
       {/* PAGINATION */}
       
@@ -109,4 +96,4 @@ const ParentListPage = () => {
   )
 }
 
-export default ParentListPage
+export default LessonListPage
